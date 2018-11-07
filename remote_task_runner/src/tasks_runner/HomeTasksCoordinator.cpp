8c5f3ff5b73b9652770e5bfea39859d44684db55
phil <dmarkov23@gmail.com>
2018-11-07 20:13:29 +0100

    created few tests

--- remote_task_runner/src/tasks_runner/HomeTasksCoordinator.cpp
+++ remote_task_runner/src/tasks_runner/HomeTasksCoordinator.cpp
@@ -1,13 +1,22 @@
 #include "HomeTasksCoordinator.h"
 
 
-
 HomeTasksCoordinator::HomeTasksCoordinator(Light &light, FileVisualizer &fileVisualizer) : light(light),
                                                                                            fileVisualizer(
                                                                                                    fileVisualizer) {}
-
-
 void HomeTasksCoordinator::TurnOnHomeLight() {
     light.TurnOn();
+}
+
+void HomeTasksCoordinator::TurnOffHomeLight() {
+    light.TurnOff();
+}
+
+void HomeTasksCoordinator::ShowFileOnScreen() {
+    fileVisualizer.ShowFile();
+}
+
+void HomeTasksCoordinator::RemoveFileFromScreen() {
+    fileVisualizer.CloseFile();
 }
 
--- remote_task_runner/test/HomeTasksCoordinatorTest.cpp
+++ remote_task_runner/test/HomeTasksCoordinatorTest.cpp
@@ -22,3 +22,28 @@ TEST_F(HomeTasksCoordinatorTest, LightsTurnedOn) {
     homeTasksCoordinator.TurnOnHomeLight();
 
 }
+
+TEST_F(HomeTasksCoordinatorTest, LightsTurnedOff) {
+    // Setup expectations
+    EXPECT_CALL(mockLight, TurnOff()).Times(1);
+
+    // Call method
+    homeTasksCoordinator.TurnOffHomeLight();
+
+}
+TEST_F(HomeTasksCoordinatorTest, FileShowedOnScreen) {
+    // Setup expectations
+    EXPECT_CALL(mockFileVisualizer, ShowFile()).Times(1);
+
+    // Call method
+    homeTasksCoordinator.ShowFileOnScreen();
+
+}
+TEST_F(HomeTasksCoordinatorTest, FileRemovedFromScreen) {
+    // Setup expectations
+    EXPECT_CALL(mockFileVisualizer, CloseFile()).Times(1);
+
+    // Call method
+    homeTasksCoordinator.RemoveFileFromScreen();
+
+}
